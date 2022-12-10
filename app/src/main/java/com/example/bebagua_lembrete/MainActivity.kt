41a3cba448b2bab53201071936a76d2ad7914561
Glauco Dante <108493608+glaucodante@users.noreply.github.com>
2022-12-10 10:58:45 -0300

    layout finish

---
+++ app/src/main/java/com/example/bebagua_lembrete/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bebagua_lembrete
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//  Escondendo a BARRA DE AÇÃO
+        supportActionBar!!.hide()
+    }
+}
